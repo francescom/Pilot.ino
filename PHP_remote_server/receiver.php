84242249994ae641cfd3a768b5d2de6b335be34e
francescom <github@eSurfers.com>
2014-01-13 16:34:34 +0100

    Update and rename index.php to receiver.php
    Added comment and chose better name

--- PHP_remote_server/receiver.php
+++ PHP_remote_server/receiver.php
@@ -1,5 +1,24 @@
 <?php
 
+
+// What is this?
+
+
+// Example script that waits, receives sensor info and sends commands to execute to a remote PC
+// or development board connected to an Arduino
+
+// The usage scenario is a development board or small PC connected to the internet from
+// inside a protected LAN with NAT or firewall. The small PC can only send GET/POST requests out
+// it can not be reached from the outside world.
+
+// Developed for UDOO board (linux+Arduino embedded)
+
+// The uploadtest script runs on the small PC and connects to this sample script on some public web server
+// The public web server can receive sensor data from the small PC and return commands to execute
+// (turn on/off or dim leds, activate motors, etc..)
+
+
+
 // get a sensor if it is listed in $SENSORS_ANALOG_READ or $SENSORS_DIGITAL_READ in sending script
 if(isset($_REQUEST['A0']))$A0=$_REQUEST['A0'];
 else $A0='?';
